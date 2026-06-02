f4ff4cfbea38526a1a3e6ee3cdac28327ed74670
rohit shah <rohitshah9958@gmail.com>
2026-06-02 23:55:36 +0530

    cloudinary api fixec

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const API_BASE = import.meta.env.VITE_API_END_POINT || "http://localhost:8000/api/v1";
+
+export const USER_API_END_POINT = `${API_BASE}/user`;
+export const JOB_API_END_POINT = `${API_BASE}/job`;
+export const APPLICATION_API_END_POINT = `${API_BASE}/application`;
+export const COMPANY_API_END_POINT = `${API_BASE}/company`;
